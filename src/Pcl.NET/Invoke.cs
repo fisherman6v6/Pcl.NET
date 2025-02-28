e4ace8b0b8fa56c21c96b0f01b673fa1785474af
Alessandro Fici <alessandro.fici@spea.com>
2025-02-28 16:53:43 +0100

    Fix nuget generation. Added SetDllImportResolver for native libraries

--- src/Pcl.NET/Invoke.cs
+++ src/Pcl.NET/Invoke.cs
@@ -1,4 +1,4 @@
-﻿using Pcl.NET.Eigen;
+﻿using System.Reflection;
 using System.Runtime.InteropServices;
 
 namespace Pcl.NET
@@ -7,9 +7,28 @@ namespace Pcl.NET
     {
         private const string DllName = "pcl-bindings";
 
+        static Invoke() => NativeLibrary.SetDllImportResolver(Assembly.GetExecutingAssembly(), OnImport);
+
+        static string GetLibraryPath(string libraryName)
+        {
+            string path = string.Empty;
+            if (RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
+            {
+                path = Path.Combine("runtimes", $"win-{RuntimeInformation.OSArchitecture}", "native", libraryName);
+            }
+            return path;
+        }
+
+        private static nint OnImport(string libraryName, Assembly assembly, DllImportSearchPath? searchPath)
+        {
+            var platFormDependantPath = GetLibraryPath(libraryName);
+            return NativeLibrary.Load(platFormDependantPath, assembly, searchPath);
+        }
+
         #region PointCloudXYZ
 
         [DllImport(DllName, CallingConvention = CallingConvention.Cdecl)]
+
         public static extern IntPtr pointcloud_xyz_ctor();
 
         [DllImport(DllName, CallingConvention = CallingConvention.Cdecl)]
@@ -440,6 +459,57 @@ namespace Pcl.NET
 
         [DllImport(DllName, CallingConvention = CallingConvention.Cdecl)]
         public static extern void cropbox_pointxyz_set_keep_organized(IntPtr ptr, int keep_organized);
+
+        #endregion
+
+        #region Matrix4f
+
+        [DllImport(DllName, CallingConvention = CallingConvention.Cdecl)]
+        public static extern IntPtr eigen_matrix4_f_ctor();
+
+        [DllImport(DllName, CallingConvention = CallingConvention.Cdecl)]
+        public static extern void eigen_matrix4_f_delete(ref IntPtr ptr);
+
+        [DllImport(DllName, CallingConvention = CallingConvention.Cdecl)]
+        public static extern float eigen_matrix4_f_get_index(IntPtr ptr, int row, int col);
+
+        [DllImport(DllName, CallingConvention = CallingConvention.Cdecl)]
+        public static extern void eigen_matrix4_f_set_index(IntPtr ptr, int row, int col, float value);
+
+        [DllImport(DllName, CallingConvention = CallingConvention.Cdecl)]
+        public unsafe static extern float* eigen_matrix4_f_data(IntPtr ptr);
+
+        #endregion
+
+        #region Vector4f
+
+        [DllImport(DllName, CallingConvention = CallingConvention.Cdecl)]
+        public static extern IntPtr eigen_vectorx_f_ctor();
+
+        [DllImport(DllName, CallingConvention = CallingConvention.Cdecl)]
+        public static extern void eigen_vectorx_f_delete(ref IntPtr ptr);
+
+        [DllImport(DllName, CallingConvention = CallingConvention.Cdecl)]
+        public static extern float eigen_vectorx_f_get_index(IntPtr ptr, long index);
+
+        [DllImport(DllName, CallingConvention = CallingConvention.Cdecl)]
+        public static extern void eigen_vectorx_f_set_index(IntPtr ptr, long index);
+
+        [DllImport(DllName, CallingConvention = CallingConvention.Cdecl)]
+        public unsafe static extern float* eigen_vectorx_f_data(IntPtr ptr);
+
+        [DllImport(DllName, CallingConvention = CallingConvention.Cdecl)]
+        public static extern void eigen_vectorx_f_normalize(IntPtr ptr);
+
+        #endregion
+
+        #region Quaternionf
+
+        [DllImport(DllName, CallingConvention = CallingConvention.Cdecl)]
+        public static extern IntPtr eigen_quaternion_f_ctor();
+
+        [DllImport(DllName, CallingConvention = CallingConvention.Cdecl)]
+        public static extern void eigen_quaternion_f_delete(ref IntPtr ptr);
 
         #endregion
     }
