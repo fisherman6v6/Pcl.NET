47ab333e50411aaf6cf9a6d486873463f0aa87bb
Alessandro Fici <alessandro.fici@spea.com>
2025-09-05 10:22:06 +0200

    Add min/max methods for PointCloudXYZI and PointCloudXYZRGBA
    Implemented new export functions in common.cpp to support the new point cloud types. Incremented project version from 0.6.5 to 0.6.6 in Pcl.NET.csproj.

--- src/Pcl.NET/Common.cs
+++ src/Pcl.NET/Common.cs
@@ -9,5 +9,21 @@
             max_pt = new PointXYZ();
             Invoke.common_get_min_max_3d_pointxyz(cloud, ref min_pt, ref max_pt);
         }
+
+        public static void GetMinMax3D(PointCloudXYZI cloud, out PointXYZI min_pt, out PointXYZI max_pt)
+        {
+            ArgumentNullException.ThrowIfNull(cloud, nameof(cloud));
+            min_pt = new PointXYZI();
+            max_pt = new PointXYZI();
+            Invoke.common_get_min_max_3d_pointxyzi(cloud, ref min_pt, ref max_pt);
+        }
+
+        public static void GetMinMax3D(PointCloudXYZRGBA cloud, out PointXYZRGBA min_pt, out PointXYZRGBA max_pt)
+        {
+            ArgumentNullException.ThrowIfNull(cloud, nameof(cloud));
+            min_pt = new PointXYZRGBA();
+            max_pt = new PointXYZRGBA();
+            Invoke.common_get_min_max_3d_pointxyzrgba(cloud, ref min_pt, ref max_pt);
+        }
     }
 }
